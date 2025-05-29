b87837e57b0361a4213c5798abcbcaf524365742
eonwy <yeawon6566@gmail.com>
2025-05-29 21:51:37 +0900

    FIX : verify member code (springboot boilerplate)

--- springboot-boilerplate/src/main/java/com/grepp/spring/app/controller/web/member/payload/SignupRequest.java
+++ springboot-boilerplate/src/main/java/com/grepp/spring/app/controller/web/member/payload/SignupRequest.java
@@ -3,6 +3,7 @@ package com.grepp.spring.app.controller.web.member.payload;
 import com.grepp.spring.app.model.member.dto.MemberDto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -12,7 +13,8 @@ public class SignupRequest {
     @NotBlank
     private String userId;
     @NotBlank
-    @Size(min = 4, max = 10)
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-zA-Z])(?=.*[^a-zA-Zㄱ-힣])(?!.*[ㄱ-힣]).{8,}$"
+        ,message = "비밀번호는 8자리 이상의 영문자, 숫자, 특수문자 조합으로 이루어져야 합니다.")
     private String password;
     @NotBlank
     @Email
---
+++ springboot-boilerplate/src/main/java/com/grepp/spring/app/controller/websocket/TeamChatController.java
@@ -0,0 +1,10 @@
+package com.grepp.spring.app.controller.websocket;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@MessageMapping("team")
+public class TeamChatController {
+
+}
