8852362f8c79c62eb65addf58e4395ae630e8d84
eonwy <yeawon6566@gmail.com>
2025-05-29 21:50:27 +0900

    TIL : 0529 - session save using redis

---
+++ springboot-jwt/src/test/java/com/grepp/spring/infra/config/RedisConfigTest.java
@@ -0,0 +1,19 @@
+package com.grepp.spring.infra.config;
+
+import com.grepp.spring.app.model.auth.RefreshTokenRepository;
+import com.grepp.spring.app.model.auth.entity.RefreshToken;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisConfigTest {
+    
+    @Autowired
+    RefreshTokenRepository refreshTokenRepository;
+    
+    @Test
+    public void testRedisTemplate(){
+        refreshTokenRepository.save(new RefreshToken("super@grepp.com"));
+    }
+}
