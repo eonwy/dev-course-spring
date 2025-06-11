a4d17dff978c1f123cf9a78c823e4ce6575af551
eonwy <yeawon6566@gmail.com>
2025-06-11 15:18:05 +0900

    TIL : 0611 - 채팅 서버 2개 가동

--- chat-service-B/src/main/kotlin/com/grepp/spring/infra/config/Configs.kt
+++ chat-service-B/src/main/kotlin/com/grepp/spring/infra/config/Configs.kt
@@ -1,6 +1,11 @@
 package com.grepp.spring.infra.config
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.grepp.spring.infra.event.EventMessageDelegate
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -15,12 +20,12 @@ import org.springframework.data.redis.listener.adapter.MessageListenerAdapter
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
-
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableRedisRepositories
-class RedisConfig(
-) {
+class RedisConfig {
     @Value("\${spring.data.redis.port}")
     private var port = 0
 
@@ -44,6 +49,18 @@ class RedisConfig(
     }
 
     @Bean
+    fun redisTemplate(
+        redisConnectionFactory: RedisConnectionFactory,
+        objectMapper: ObjectMapper
+    ): RedisTemplate<String, String> {
+        val redisTemplate = RedisTemplate<String, String>()
+        redisTemplate.connectionFactory = redisConnectionFactory
+        redisTemplate.keySerializer = StringRedisSerializer()
+        redisTemplate.valueSerializer = GenericJackson2JsonRedisSerializer(objectMapper)
+        return redisTemplate
+    }
+
+    @Bean
     fun messageListenerAdapter(listener: EventMessageDelegate): MessageListenerAdapter {
         return MessageListenerAdapter(listener, "handleMessage")
     }
@@ -55,19 +72,36 @@ class RedisConfig(
     ): RedisMessageListenerContainer {
         val container = RedisMessageListenerContainer()
         container.setConnectionFactory(connectionFactory)
-        container.addMessageListener(listener, ChannelTopic.of("user-service"))
+        container.addMessageListener(listener, ChannelTopic.of("chat"))
         return container
+    }
+}
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/")
+            .setCachePeriod(3600 * 24 * 30)
     }
 
     @Bean
-    fun redisTemplate(
-        redisConnectionFactory: RedisConnectionFactory,
-        objectMapper: ObjectMapper
-    ): RedisTemplate<String, String> {
-        val redisTemplate = RedisTemplate<String, String>()
-        redisTemplate.connectionFactory = redisConnectionFactory
-        redisTemplate.keySerializer = StringRedisSerializer()
-        redisTemplate.valueSerializer = GenericJackson2JsonRedisSerializer(objectMapper)
-        return redisTemplate
+    fun objectMapper(): ObjectMapper {
+        val kotlinModule = KotlinModule.Builder()
+            .withReflectionCacheSize(512)
+            .configure(KotlinFeature.NullToEmptyCollection, false)
+            .configure(KotlinFeature.NullToEmptyMap, false)
+            .configure(KotlinFeature.NullIsSameAsDefault, false)
+            .configure(KotlinFeature.SingletonSupport, false)
+            .build()
+
+        return ObjectMapper()
+            .registerModules(kotlinModule, JavaTimeModule())
+            .configure(SerializationFeature.FAIL_ON_UNWRAPPED_TYPE_IDENTIFIERS, false)
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            .enable(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE)
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
     }
 }
+
+
